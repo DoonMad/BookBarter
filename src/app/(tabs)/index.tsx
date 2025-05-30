2f59dbc9ed93d6970195045661070eec6c65ca25
Apoorva Mundada <apoorva.aam@gmail.com>
2025-05-30 20:00:37 +0530

    changed BookListItem design to vertical

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,36 +1,15 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
-import books, { Book } from '@/assets/data/books';
+import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
+import books from '@/assets/data/books';
 import BookListItem from '@/src/components/BookListItem';
-
-const book = books[0]
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <BookListItem book= {books[1]} />
-      <BookListItem book= {books[0]} />
-    </View>
+      <FlatList
+        data={books}
+        renderItem={({item}) => <BookListItem book= {item} />}
+        numColumns={2}
+        contentContainerStyle={{gap: 8, padding: 8}}
+        columnWrapperStyle={{gap: 8}}
+      />
   );
 }
-
-const styles = StyleSheet.create({
-  image:{
-    width: 100,
-    aspectRatio: 1
-  },
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#F1F1F1',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
